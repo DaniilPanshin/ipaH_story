cb1438cb9e40a50a08c8777008d4e0330e384e3c
Yulia-Yakovleva <49680968+Yulia-Yakovleva@users.noreply.github.com>
2020-09-02 03:07:52 +0300

    Add files via upload

--- src/take_IpaH_features_coords.py
+++ src/take_IpaH_features_coords.py
@@ -19,16 +19,17 @@ def check_q_values(f, key_words):
                 return True
 
 
-
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Write all IpaH features to table with coordinates')
     parser.add_argument('-i', '--input', type=str, nargs='*', help='Path to .gbff files')
+    parser.add_argument('-o', '--output', type=str, default='IpaH_features.tsv',
+                        help='Path to output file. By default file will be written to current working directory under "IpaH_features.tsv" name')
     args = parser.parse_args()
 
     files = args.input
     key_words = ('IpaH', 'ipaH', 'invasion plasmid antigen')
-    df = pd.DataFrame(columns=['AssemblyID', 'GenBankID', 'FeatureName', 'FeatureStart', 'FeatureEnd'])
+    df = pd.DataFrame(columns=['AssemblyID', 'GenBankID', 'ChrType', 'FeatureName', 'FeatureStart', 'FeatureEnd'])
 
     count = 0
     for file in files:
@@ -37,34 +38,39 @@ if __name__ == "__main__":
         recs = SeqIO.parse(file, 'genbank')
         for rec in recs:
             if 'plasmid' in rec.description:
-                pass
+                chr_type = 'Plasmid'
             else:
-                gb_id = rec.name
-                print(f"Processing... {count}/{len(files)}\t{gb_id}")
+                chr_type = 'Chromosome'
+            gb_id = rec.name
+            print(f"Processing... {count}/{len(files)}\t{assembly_id}")
 
-                chromosomal_features = []
-                for f in rec.features:
-                    if check_CDS_and_genes(f, key_words) or check_q_values(f, key_words):
-                        chromosomal_features.append(f)
+            chromosomal_features = []
+            for f in rec.features:
+                if check_CDS_and_genes(f, key_words) or check_q_values(f, key_words):
+                    chromosomal_features.append(f)
 
-                # print(len(set(chromosomal_features)))
-                for feat in set(chromosomal_features):
-                    f_start = feat.location.start
-                    f_end = feat.location.end
-                    if feat.type == 'CDS' or feat.type == 'gene':
-                        if 'gene' in feat.qualifiers:
-                            f_name = feat.qualifiers['gene'][0]
-                        elif 'product' in feat.qualifiers:
-                            f_name = feat.qualifiers['product'][0]
-                    else:
-                        f_name = feat.qualifiers['note'][0]
-                    df = df.append({'AssemblyID': assembly_id,
-                                    'GenBankID': gb_id,
-                                    'FeatureName': f_name,
-                                    'FeatureStart': f_start,
-                                    'FeatureEnd': f_end},
-                                   ignore_index=True)
-    df = df.sort_values(by=['AssemblyID', 'FeatureStart'])
-    df = df.drop_duplicates()
-    df.to_csv('IpaH_features.tsv', sep='\t')
-    print(f"Output is written to {os.getcwd()}/IpaH_features.tsv file :>")
+            # print(len(set(chromosomal_features)))
+            for feat in set(chromosomal_features):
+                f_start = feat.location.start
+                f_end = feat.location.end
+
+                if feat.type == 'CDS' or feat.type == 'gene':
+                    if 'gene' in feat.qualifiers:
+                        f_name = feat.qualifiers['gene'][0]
+                    elif 'product' in feat.qualifiers:
+                        f_name = feat.qualifiers['product'][0]
+                else:
+                    f_name = feat.qualifiers['note'][0]
+
+                df = df.append({'AssemblyID': assembly_id,
+                                'GenBankID': gb_id,
+                                'FeatureName': f_name,
+                                'ChrType': chr_type,
+                                'FeatureStart': f_start,
+                                'FeatureEnd': f_end},
+                               ignore_index=True)
+
+    df = df.sort_values(by=['AssemblyID', 'ChrType', 'FeatureStart'])
+    df = df.drop_duplicates().reset_index(drop=True)
+    df.to_csv(args.output, sep='\t')
+    print(f"Output is written to {os.getcwd()}/{args.output} file :>")
